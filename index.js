2a9d508a564da8d78efd0ca7d18642e3a512ce44
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-07-06 01:04:49 +0530

    Cluster Added

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 require("dotenv").config()
+const cluster = require("cluster");
+const os = require("os");
 const express = require("express");
-const app = express();
+
 const {ConnectDB} = require ("./lib/db");
 const UserRouter = require ("./routes/user.route")
 const StaticRouter = require ("./routes/static.route");
 const cookieParser = require("cookie-parser");
 
-ConnectDB("mongodb://127.0.0.1:27017/password-Hashed").then(() => console.log("Mongodb running..."))
+const numCPUs = os.availableParallelism();
 
 
 
-app.use(express.json());
-app.use(cookieParser());
+if(cluster.isPrimary){
+    console.log(`Primary ${process.pid} is running`)
+    for(let i=0; i<numCPUs; i++){
+        cluster.fork();
+    }
+}
+else{
+    const app = express();
+    ConnectDB("mongodb://127.0.0.1:27017/password-Hashed").then(() => console.log("Mongodb running..."))
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use("/", StaticRouter);
+    app.use("/user", UserRouter);
+    app.listen(process.env.PORT, () => {
+        console.log(`Worker ${process.pid} listening on port ${process.env.PORT}`);
+    });
+}
 
 
-app.use("/", StaticRouter)
-app.use("/user", UserRouter);
-
-
-
-
-
-app.listen(process.env.PORT, () => console.log("Server Running..."))
